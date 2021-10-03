149d76a47ead97d0684318c305faf3e55db00288
Freddie Perez <fperez@ida.org>
2021-10-03 13:53:46 -0400

    Package instance and viz examples
    Signed-off-by: Freddie Perez <fperez@ida.org>

---
+++ deloitte/deloitte.py
@@ -0,0 +1,10 @@
+#!/home/fperez/miniconda3/envs/deloitte/bin/python
+"""
+This module contains the scripts and calls used to examine the 
+Pike's Peek 10K datasets for the Deloitte Data Exercise.
+"""
+
+import pandas as pd
+import numpy as pd
+
+
