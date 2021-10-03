49ef07db46c6e9728b3421da4ccf2d7aa7143fc6
Freddie Perez <fperez@ida.org>
2021-10-03 17:36:54 -0400

    Updating scripts
    Signed-off-by: Freddie Perez <fperez@ida.org>

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Deloitte Interview: Data Exercise Section
+
+Use the following cmd in the root directory `deloitte/` to install the Deloitte package
+    `pip install -e .`
+
--- deloitte/deloitte.py
+++ deloitte/deloitte.py
@@ -4,7 +4,45 @@ This module contains the scripts and calls used to examine the
 Pike's Peek 10K datasets for the Deloitte Data Exercise.
 """
 
+import string
+import re
 import pandas as pd
-import numpy as pd
+import numpy as np
+class Deloitte:
+    """
+    A class that contains all of the scripts used for the exercise
+    """
+
+    def __init__(self, file_path):
+        self.file_path = file_path 
+        self.f_df = None
+        self.m_df = None
+        self.all_df = None
 
 
+
+    def clean_data(self):
+        raw_data = pd.read_csv(
+                'data/raw/{}'.format(self.file_path),\
+                sep='\t')
+        
+        # Renaming column names
+        raw_data.columns = map(str.lower, raw_data.columns)
+        raw_data.rename(columns=\
+                {'div/tot':'div_total', 'ag':'age',\
+                'gun tim':'gun_time', 'net tim':'net_time'},\
+                inplace=True\
+                )
+        clean_cols = raw_data.columns.tolist()
+        clean_cols.pop(3) # Removes `name` column
+        clean_cols.pop(4) # Removes `hometown` column
+
+        for col in clean_cols:
+            raw_data[col].replace(\
+                    to_replace='[#*^a-zA-Z ]',\
+                    value='',\
+                    regex=True, 
+                    inplace=True)
+        raw_data['hometown'].replace(to_replace='[,.]', value='', regex=True, inplace=True)
+
+        return raw_data
